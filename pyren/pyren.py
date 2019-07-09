6422a48d4038cfa45fd353d46accee23eb1a16be
Ashwin Rao <ashwin.rao@getcruise.com>
2019-07-09 13:20:29 -0700

    Add argument parsing

--- pyren/pyren.py
+++ pyren/pyren.py
@@ -1,7 +1,19 @@
+import argparse
+
+def parse_args():
+  """ Parse arguments for program
+  """
+    parser = argparse.ArgumentParser(description="Program for renaming files")
+    parser.add_argument("find",help="Find pattern")
+    parser.add_argument("-i","--ignore_case", action="store_true",  help = "Ignore case")
+    parser.add_argument("rename",help="Rename pattern")
+    parser.add_argument("-d","--directory", help = "Directory")
+    parser.add_argument("-y","--no_confirm", action="store_true", help="Don't ask for confirmation")
+
+    return parser.parse_args()
+
 def main():
-
-  print("Hello World")
-
+  parse_args()
 
 if __name__ == "__main__":
   main()
