60cf48f5ad622632067cd26fcaf6c54d93733af8
ashwinmr <iashwinrao@gmail.com>
2019-07-09 23:48:08 -0700

    Complete functionality

--- pyren/pyren.py
+++ pyren/pyren.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import re
 
 def parse_args():
   """ Parse arguments for program
@@ -41,12 +42,45 @@ def disp_res(ren_list):
 def main():
 
   # Parse arguments
-  # args = parse_args()
-  ren_list = []
-  for f in os.listdir("./temp"):
-    ren_list.append((f,f))
+  args = parse_args()
 
+  find_pat = args.find
+  ren_pat = args.rename
+  directory = args.directory
+
+  # Set icase flag
+  if args.ignore_case:
+    icase = re.I
+  else:
+    icase = 0
+
+  # Create list of old and new paths
+  ren_list = []
+
+  # Create regex object
+  ro = re.compile(pattern = find_pat, flags = icase)
+
+  # Create rename list for files in directory
+  for old_str in os.listdir(directory):
+
+    # Check for matching file
+    if(ro.match(string = old_str)):
+
+      # Create rename string by using patterns
+      new_str = ro.sub(repl = ren_pat, string = old_str, count=1)
+
+      # Store results
+      ren_list.append((os.path.join(directory,old_str), os.path.join(directory,new_str)))
+
+  # Display results
   disp_res(ren_list)
+
+  # Ask for confirmation before renaming
+  if not args.no_confirm:
+    if(prompt()):
+      perf_ren(ren_list)
+  else:
+    perf_ren(ren_list)
 
 if __name__ == "__main__":
   main()
