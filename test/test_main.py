98c0b28d9ef8ccbb105423394277bd494e436419
ashwinmr <iashwinrao@gmail.com>
2019-07-10 22:50:42 -0700

    Add tests

--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,28 @@
 from pyren import pyren
 
 def test_disp_res(capfd):
-    ren_list = [("foo","bar")]
+    """ Test disp_res functions
+    """
+    ren_list = [("./foo","./bar")]
     pyren.disp_res(ren_list)
+
+    # Read console output
     out, _ = capfd.readouterr()
-    assert out == "{:30}{:30}".format("foo","bar")
+    assert out == "{:30}{:30}\n".format("foo","bar")
+
+def test_prompt_no(monkeypatch):
+    """ Test that the prompt works for n
+    """
+    # monkeypatch the "input" function, so that it returns "n".
+    monkeypatch.setattr('builtins.input', lambda x: "n")
+
+    assert pyren.prompt() == False
+
+def test_prompt_yes(monkeypatch):
+    """ Test that the prompt works for y
+    """
+    # monkeypatch the "input" function, so that it returns "n".
+    monkeypatch.setattr('builtins.input', lambda x: "y")
+
+    assert pyren.prompt() == True
+
